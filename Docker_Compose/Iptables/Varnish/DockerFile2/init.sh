0b984fb4619aca92bd91f5fa7cf48271bd80c3a2
ricardo121 <ricardoasimbaya@gmail.com>
2018-11-04 17:40:05 +0100

    docker

---
+++ Docker_Compose/Iptables/Varnish/DockerFile2/init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+exec varnishd \
+    -F \
+    -f ${VARNISH_CONFIG} \
+    -s ${VARNISH_STORAGE} \
+    -a ${VARNISH_LISTEN} \
+    -T ${VARNISH_MANAGEMENT_LISTEN} \
+    ${VARNISH_DAEMON_OPTS}
+
