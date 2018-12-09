927bc8f9b20ed8fe6047902e6f6ac77e193ef7f7
ricardo121 <ricardoasimbaya@gmail.com>
2018-12-09 21:09:06 +0100

    Docker

--- Docker_Compose/Varnish2/Varnish/DockerFile/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-exec varnishd \
-    -F \
-    -f ${VARNISH_CONFIG} \
-    -s malloc,${VARNISH_STORAGE} \
-    -a ${VARNISH_LISTEN} \
-    -T localhost:${VARNISH_MANAGEMENT_LISTEN} \
-    ${VARNISH_DAEMON_OPTS}
-
